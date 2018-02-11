dfaf7c77763f0b5942cb2f7c90df08dbadd6363f
Kimarie Haverty-Bozman <34926895+KimmyBeee@users.noreply.github.com>
2018-02-10 16:17:04 -0800

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,12 @@
+```JavaScript
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'qPO7zOcVgNTY4vgCcZlImSgSv',
+  consumer_secret: 'WyonhT8osre9EBKLnbAobiysf9VDDyMx3MangoIRg1tU6H98kn',
+  access_token_key: '16195162-dNAK6mBuPnyEJWtmqf82iqk4TPoVrYUM5QcBF7QMN',
+  access_token_secret: 'rH8lObhIWQHvlhc1QiGH1G4HAYV5PXtivWZXWkIduU5sk',
+}
+
+module.exports = twitterKeys;
+```
